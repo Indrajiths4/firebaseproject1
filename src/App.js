a22a49f5c2e352baf3a1133b01a7845d4b8ece79
Indrajiths4 <95161630+Indrajiths4@users.noreply.github.com>
2024-03-10 16:59:21 +0530

    updateDoc

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import './App.css';
 import {useState} from "react";
-import {app} from './firebaseConfig'
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-function App() {
-  let auth = getAuth();
-  const [data, setData] = useState({});
+import {app, database} from './firebaseConfig'
+import {collection, addDoc, getDocs, doc, updateDoc} from 'firebase/firestore'
 
+function App() {
+  
+  const [data, setData] = useState({});
+  const collectionRef = collection(database, "users");
   const handleInput = (event) => {
     let newInput = { [event.target.name]: event.target.value};
 
     setData({ ...data, ...newInput });
   };
-
+ 
   const handleSubmit = () => { 
-    createUserWithEmailAndPassword(auth, data.email, data.password)
-    .then((response) => {
-      console.log(response.user)
+    addDoc(collectionRef, {
+      email: data.email,
+      password: data.password
+    })
+    .then(() => {
+      alert('Data Added')
     })
     .catch((err) => {
-      alert(err.message)
+      alert('Error')
     })
   };
+  const getData = () => {
+    getDocs(collectionRef).then((response) => {
+      console.log(response.docs.map((item => {
+        return {...item.data(),id: item.id};
+      })))
+    })
+  };
+
+  const updateData = () => {
+    const docToUpdate = doc(database,'users','3bxIQmvYfyfaFZZyZWu2')
+    updateDoc(docToUpdate, {
+      email: 'ABC',
+      password: 123
+    })
+  };
+
   return (
     <div className="App">
       
         <input name="email" placeholder='Email' onChange={(event) =>handleInput(event)}></input>
         <input name='password' placeholder='Password' onChange={(event) => handleInput(event)}></input>
         <br></br>
-        <button onClick={handleSubmit}>Submit</button>
+        <button onClick={updateData}>Submit</button>
       
     </div>
   );
