ce69dab2582583105ca537afdae14bc2d0c9bef5
Indrajiths4 <95161630+Indrajiths4@users.noreply.github.com>
2024-03-10 15:38:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
+import {app} from './firebaseConfig'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 function App() {
+  let auth = getAuth();
+  const [data, setData] = useState({});
+
+  const handleInput = (event) => {
+    let newInput = { [event.target.name]: event.target.value};
+
+    setData({ ...data, ...newInput });
+  };
+
+  const handleSubmit = () => { 
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+    .then((response) => {
+      console.log(response.user)
+    })
+    .catch((err) => {
+      alert(err.message)
+    })
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <input name="email" placeholder='Email' onChange={(event) =>handleInput(event)}></input>
+        <input name='password' placeholder='Password' onChange={(event) => handleInput(event)}></input>
+        <br></br>
+        <button onClick={handleSubmit}>Submit</button>
+      
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2k6w638YPYURvNagpBc6j_bAHpVUjC0o",
+  authDomain: "fir-frontend-458bd.firebaseapp.com",
+  projectId: "fir-frontend-458bd",
+  storageBucket: "fir-frontend-458bd.appspot.com",
+  messagingSenderId: "771246768967",
+  appId: "1:771246768967:web:beb44c735db24e00104262"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
