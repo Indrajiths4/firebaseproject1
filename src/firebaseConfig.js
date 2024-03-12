af32676271d0129724865a5686939572e076ec4c
Indrajith S Nair <95161630+Indrajiths4@users.noreply.github.com>
2024-03-13 01:41:35 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,19 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB2k6w638YPYURvNagpBc6j_bAHpVUjC0o",
-  authDomain: "fir-frontend-458bd.firebaseapp.com",
-  projectId: "fir-frontend-458bd",
-  storageBucket: "fir-frontend-458bd.appspot.com",
-  messagingSenderId: "771246768967",
-  appId: "1:771246768967:web:beb44c735db24e00104262"
-};
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const database = getFirestore(app)
